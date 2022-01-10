00003dca13f7872043dbe05df325c0152d77396c
ALI <1416978277@qq.com>
2022-01-10 17:43:59 +0800

    0110

---
+++ 004-vue-cli基础/002-setup组合式API/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>普通：{{ count }}</h1>
+  <button @click="countAdd">+1</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    countAdd() {
+      this.count++;
+    }
+  },
+  components: {
+    HelloWorld
+  },
+}
+</script>
