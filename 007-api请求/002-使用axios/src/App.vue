4bfedfa0763e2dd6261b6681348aeda76dcd46eb
ALI <1416978277@qq.com>
2022-01-14 17:55:33 +0800

    0114

---
+++ 007-api请求/002-使用axios/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <HelloWorld msg="Hello Vue 3.0 + Vite"/>
+  <button @click="get">请求</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    // let api = 'http://httpbin.org'
+    let api = 'https://api.apiopen.top/getJoke'
+
+    function get() {
+      // es6可以通过fetch来请求
+      fetch(api).then(res => res.json()).then(data => {
+        console.log(data);
+      }).catch(err => {
+        console.log(err);
+      })
+    }
+
+    return {get}
+  }
+}
+</script>
