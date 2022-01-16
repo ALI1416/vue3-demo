035f2841d1022f74b7025b962b1aea597075523c
ALI <1416978277@qq.com>
2022-01-16 21:54:20 +0800

    0116

---
+++ 008-element-plus/001-base/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-button disabled>Default</el-button>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ 008-element-plus/element-plus安装.md
@@ -0,0 +1,5 @@
+# 安装element-plus
+`npm install element-plus --save`
+
+## 自动导入
+`npm install -D unplugin-vue-components unplugin-auto-import`
