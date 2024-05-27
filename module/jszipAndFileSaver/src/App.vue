883e1409e901ce725ba983c9ad9751150dc57f26
ALI <1416978277@qq.com>
2024-05-27 10:17:32 +0800

    0527

--- module/jszipAndFileSaver/src/App.vue
+++ module/jszipAndFileSaver/src/App.vue
@@ -6,22 +6,25 @@
 import JSZip from 'jszip'
 import FileSaver from 'file-saver'
 
-const zip = new JSZip();
+const zip = new JSZip()
 
 async function download() {
   // 抓取文件
-  const logoBlob = await fetch("favicon.ico").then(res => res.blob());
+  const logoBlob = await fetch('favicon.ico').then(res => res.blob())
   // 装载文件
-  const time = new Date().getTime();
-  const folder = zip.folder(time.toString());
-  folder.file("logo.png", logoBlob, {binary: true});
-  const helloFolder = folder.folder("hello");
-  for (let i = 0; i < 100; i++) {
-    helloFolder.file("Hello" + i + ".txt", "Hello World " + i);
+  const time = new Date().getTime()
+  const folder = zip.folder(time.toString())
+  folder.file('logo.png', logoBlob, {binary: true})
+  const helloFolder = folder.folder('hello')
+  for (let i = 0; i < 10; i++) {
+    helloFolder.file('Hello' + i + '.txt', 'Hello World ' + i)
   }
   // 压缩文件
-  const zipFile = await zip.generateAsync({type: "blob"});
+  const zipFile = await zip.generateAsync({type: 'blob'})
+  // 解压文件
+  const unzipFile = await zip.loadAsync(zipFile)
+  console.log(unzipFile)
   // 下载
-  FileSaver.saveAs(zipFile, time + ".zip");
+  FileSaver.saveAs(zipFile, time + '.zip')
 }
 </script>
