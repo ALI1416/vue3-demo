c52b3fcb32e13b62dddcb18eac7b384ffb3d86fd
ALI <1416978277@qq.com>
2024-06-04 11:34:59 +0800

    0604

---
+++ module/js-bullets/js-bullets.md
@@ -0,0 +1,4 @@
+# js-bullets 弹幕
+
+- `npm install --save js-bullets`
+- https://github.com/hugeorange/BulletJs
---
+++ module/js-bullets/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="bullet" style="width:500px;height:50px;border:1px solid"></div>
+</template>
+
+<script setup>
+import BulletJs from 'js-bullets'
+import {onMounted} from 'vue'
+
+onMounted(() => {
+  let bullet = new BulletJs('#bullet', {
+    pauseOnClick: true,
+    pauseOnHover: true
+  })
+  let msg = [
+    '哈哈哈',
+    '爱上对方过后就哭了',
+    '1234567890'
+  ]
+  let i = 0
+  setInterval(() => {
+    bullet.push(msg[i++])
+    if (i === msg.length) {
+      i = 0
+    }
+  }, 1000)
+})
+</script>
