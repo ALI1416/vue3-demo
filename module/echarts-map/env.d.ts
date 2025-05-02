7880b11460bdbe44c4f49be38f8b210b9bc290d2
ALI <1416978277@qq.com>
2025-05-02 15:47:00 +0800

    0502

---
+++ module/echarts-map/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+// https://vite.dev/guide/env-and-mode
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+  /**
+   * APP标题
+   */
+  readonly VITE_APP_TITLE: string
+}
---
+++ module/echarts-map/src/App.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>App</h1>
+  <HelloWorld/>
+</template>
